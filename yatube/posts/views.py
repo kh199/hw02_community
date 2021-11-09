8886ef37d54348263a7fcc01916e7bb6e8bc395d
Екатерина Каричева <yek.cari4ewa@yandex.ru>
2021-11-09 12:03:40 +0300

    First iteration

---
+++ yatube/posts/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
+
+
+def index(request):
+    # template = 'posts/index.html'
+    title = 'Последние обновления на сайте'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'title': title,
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
+
+
+def group_posts(request, slug):
+    # template = 'posts/group_list.html'
+    group = get_object_or_404(Group, slug=slug)
+    title = f'Записи сообщества {str(group)}'
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'title': title,
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
